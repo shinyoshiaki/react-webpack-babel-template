65e1731f6d89ae716ffd643cc708376d8b26e180
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-12-01 16:50:18 +0900

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,20 @@ module.exports = {
         }
       },
       {
+        test: /\.(ts|tsx)$/,
+        enforce: "pre",
+        use: [
+          {
+            options: {
+              eslintPath: require.resolve("eslint")
+            },
+            loader: require.resolve("eslint-loader")
+          }
+        ],
+        exclude: /node_modules/
+      },
+
+      {
         test: /\.worker\.ts$/,
         use: [
           {
