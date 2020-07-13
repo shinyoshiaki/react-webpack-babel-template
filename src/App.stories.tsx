c8833ed3fd77c9bf55c62f54cd395eb7e7195630
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-07-13 23:37:27 +0900

    fix storybook

---
+++ src/App.stories.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default { title: "Demo" };
+
+export const button = () => <button>button</button>;
--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App: FC = () => {
   return (
     <div>
       <p>{msg}</p>
-      <button>button</button>
+      <button onClick={() => setMsg("clicked")}>button</button>
     </div>
   );
 };
